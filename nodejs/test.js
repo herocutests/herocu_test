250675606f603297de9b85cc8ddc6f7c2e29fbe8
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-27 19:28:40 +0300

    node server

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,1 +1,9 @@
-return 'test 123';
+var http = require('http');
+var port = 8888;
+
+var server = http.createServer(function(req, res) {
+    // Check for notices from PHP
+        return '123';
+});
+
+server.listen(port);

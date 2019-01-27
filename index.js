61da3ff1de049e3c3ac61d36365e810431695361
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-27 21:08:23 +0300

    1212

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
+var express = require("express");
 var app = express();
+app.use(express.logger());
 
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.listen(port, function () {
- console.log(`Example app listening on port `+port);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });

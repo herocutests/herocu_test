c585b82f39e75aa022310eda527e6f554bc7f220
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-27 19:33:31 +0300

    index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var http = require('http');
-var port = 8888;
-
-var server = http.createServer(function(req, res) {
-    res.end("Hello from Express!");
-});
-
-server.listen(port);
+const http = require('http')
+const port = 3000
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
+}
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})

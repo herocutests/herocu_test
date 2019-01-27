fc9702ba3f77f35870fa5e3292d542ef98f18bee
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-27 22:46:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const http = require('http');
-const hostname = 'agnia.herokuapp.com';
-const port = Number(process.env.PORT || 3000);
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('some text '+req.headers.host );
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.end('Hello!') )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);

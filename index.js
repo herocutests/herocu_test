e3bb913541e417204f6f7d7bc8bd66d4a6712e89
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-28 00:43:06 +0300

    123

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express = require('express');
-//const SocketServer = require('ws').Server;
+var io = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
 const server = express()
-  .use((req, res) => res.end('Hello!') )
+  .use((req, res) => res.sendFile(__dirname + '/public/index.html') )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-/*
-const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+var socket = io.listen(server);
+var countUsers = 0;
+var history = [];
+var count = 0;
+var max = 100;
+
+socket.on('connect', function(client){ 
+	countUsers++;
+    socket.emit('usersCount', countUsers);
+
+    client.emit('chatHistory', {'chat' : history, users : countUsers});
+
+    client.on('addMessage', function(data) {
+    	sendMessage(data);
+    });
+
+    client.on('disconnect',function(){
+        countUsers--;
+		socket.emit('usersCount', countUsers);
+    });
+
+    client.emit('serverResp', '1234');
+
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);*/
+var sendMessage = function(data) {
+	var dt = {'user': data.user, 'msg' : data.message, 'date' : Date.now()};
+    history.push(dt);
+    socket.emit('addMessage', dt);
+}

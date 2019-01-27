6ee28e49efbf5221c19b495634011dfd43cac445
SebrinLouve <Sebrin.Louve@gmail.com>
2019-01-27 19:54:53 +0300

    test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var app = express.createServer();
-app.listen();
-return 'Server running on %s', app.address().port;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

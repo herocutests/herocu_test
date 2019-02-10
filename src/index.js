75d6a6d2e4773ed688a058c2a43dbb13032a1aa1
SebrinLouve <Sebrin.Louve@gmail.com>
2019-02-10 22:11:04 +0300

    Revert "2"
    This reverts commit 1fa6b0b0508de0f4993af189b0c4528be125ea97.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Linkify from 'linkifyjs/react';
 
 const appSection = document.getElementById('chatApp')
 const socket = io.connect(window.location.hostname, {reconnect: true});
-//window.location.hostname
 
 const helperFunctions = {
 	isEnterButton : (e) => {

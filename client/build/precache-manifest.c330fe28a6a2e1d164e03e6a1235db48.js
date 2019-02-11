9f4cb59e0f72dfebd1948bcd53ebd4135bf77473
SebrinLouve <Sebrin.Louve@gmail.com>
2019-02-11 03:46:19 +0300

    1

---
+++ client/build/precache-manifest.c330fe28a6a2e1d164e03e6a1235db48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9e2a03542987a962c12e",
+    "url": "/static/js/main.9e2a0354.chunk.js"
+  },
+  {
+    "revision": "b9a5b4a675d75e153f15",
+    "url": "/static/js/2.b9a5b4a6.chunk.js"
+  },
+  {
+    "revision": "9e2a03542987a962c12e",
+    "url": "/static/css/main.fd3c4ad3.chunk.css"
+  },
+  {
+    "revision": "97c30af04c2702e0f7db5216b4472875",
+    "url": "/index.html"
+  }
+];
